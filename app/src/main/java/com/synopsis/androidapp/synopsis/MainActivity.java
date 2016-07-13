2456677f5d4fc528c11f82c80fb5590ec1416370
User <jobin@wiztelsys.com>
2016-07-13 12:30:40 +0530

    completed registerpage

--- app/src/main/java/com/synopsis/androidapp/synopsis/MainActivity.java
+++ app/src/main/java/com/synopsis/androidapp/synopsis/MainActivity.java
@@ -1,13 +1,22 @@
 package com.synopsis.androidapp.synopsis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.start_page);
+
     }
+public void registerfn(View view)
+{
+    startActivity( new Intent(MainActivity.this, RegisterUserClass.class));
+}
 }
---
+++ app/src/main/java/com/synopsis/androidapp/synopsis/RegisterUserClass.java
@@ -0,0 +1,17 @@
+package com.synopsis.androidapp.synopsis;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+
+/**
+ * Created by User on 7/13/2016.
+ */
+public class RegisterUserClass extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_user_page);
+    }
+}
