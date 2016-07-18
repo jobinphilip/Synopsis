1e4f6ba8cb8f69bae485b7568cf94aa659e63099
User <jobin@wiztelsys.com>
2016-07-18 18:33:35 +0530

    project works

--- app/src/main/java/com/synopsis/androidapp/synopsis/MainActivity.java
+++ app/src/main/java/com/synopsis/androidapp/synopsis/MainActivity.java
@@ -3,6 +3,7 @@ package com.synopsis.androidapp.synopsis;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.facebook.CallbackManager;
@@ -12,25 +13,59 @@ import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements  GoogleApiClient.ConnectionCallbacks,  GoogleApiClient.OnConnectionFailedListener,View.OnClickListener
+        {
 
     private TextView info;
+    TextView tv_username;
     private LoginButton loginButton;
+    GoogleApiClient mGoogleApiClient;
+    private static final int RC_SIGN_IN = 9001;
     private CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
             FacebookSdk.sdkInitialize(getApplicationContext());
-            AppEventsLogger.activateApp(this);
+
+        Log.d("jobin","test1");
+            AppEventsLogger.activateApp(getApplicationContext());
+        Log.d("jobin","test1");
         callbackManager = CallbackManager.Factory.create();
+        Log.d("jobin","test1");
 
         setContentView(R.layout.start_page_5);
+
+        //code for google sign in starts
+        tv_username= (TextView) findViewById(R.id.tv_username);
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+        findViewById(R.id.btn_logout).setOnClickListener(MainActivity.this);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */,  this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        //code for google sign in ends
+
+
 
         info = (TextView)findViewById(R.id.info);
         loginButton = (LoginButton)findViewById(R.id.login_button);
@@ -60,9 +95,77 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+////////////////////////////google sign in and sign out methods start///////////////////////////////////////
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.sign_in_button:
+
+                signIn();
+
+                break;
+            case R.id.btn_logout:
+
+                signOut();
+
+                break;
+        }
+
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void signOut() {
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+
+                    }
+                });
+    }
+
+
+    private void handleSignInResult(GoogleSignInResult result) {
+
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            tv_username.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
+
+        } else {
+            // Signed out, show unauthenticated UI.
+            // updateUI(false);
+        }
+    }
+   /* @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+
+    }*/
+
+    ////////////////////////////google sign in and sign out methods end///////////////////////////////////////
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
+
+        //////////////////////below code added for google signin/////////////
+       super.onActivityResult(requestCode, resultCode, data);
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+
+        //////////////////////above code added for google signin/////////////
     }
 
     public void loginfn(View view)
@@ -73,4 +176,20 @@ public class MainActivity extends AppCompatActivity {
     {
         startActivity(new Intent(MainActivity.this,RegisterUserClass.class));
     }
-}
+
+
+            @Override
+            public void onConnected(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onConnectionSuspended(int i) {
+
+            }
+
+            @Override
+            public void onConnectionFailed(ConnectionResult connectionResult) {
+
+            }
+        }
